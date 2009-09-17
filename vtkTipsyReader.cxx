f65537d7373d972cb2b4fc67427bbbadbc55f324
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-09-17 15:54:48 +0000

    further work, enabling all particle's being read

--- vtkTipsyReader.cxx
+++ vtkTipsyReader.cxx
@@ -11,7 +11,7 @@ Currently only reads in standard format Tipsy files
 #include "vtkPolyData.h"
 #include "vtkPoints.h"
 #include "vtkCellArray.h"
-#include "vtkDoubleArray.h"
+#include "vtkFloatArray.h"
 #include "vtkSmartPointer.h"
 //Initializing for reading  
 ifTipsy in;          // The input file
@@ -19,12 +19,17 @@ TipsyHeader       h; // The header structure
 TipsyGasParticle  g; // A gas particle
 TipsyDarkParticle d; // A dark particle
 TipsyStarParticle s; // A star particle
-//points and scalars
+//points, scalars, and vectors
 vtkSmartPointer<vtkPoints> points;
 vtkSmartPointer<vtkCellArray> verts; 
-vtkSmartPointer<vtkDoubleArray> mass_scalars;
-double x[3]; //for positions
-double v[3]; //for velocities
+vtkSmartPointer<vtkFloatArray> mass_scalars;
+vtkSmartPointer<vtkFloatArray> phi_scalars;
+vtkSmartPointer<vtkFloatArray> rho_scalars;        
+vtkSmartPointer<vtkFloatArray> temp_scalars;       
+vtkSmartPointer<vtkFloatArray> hsmooth_scalars;    
+vtkSmartPointer<vtkFloatArray> metals_scalars;
+vtkSmartPointer<vtkFloatArray> eps_scalars;
+vtkSmartPointer<vtkFloatArray> tform_scalars;
 uint32_t i;
 vtkCxxRevisionMacro(vtkTipsyReader, "$Revision: 1.0 $");
 vtkStandardNewMacro(vtkTipsyReader);
@@ -34,13 +39,44 @@ vtkTipsyReader::vtkTipsyReader()
 {
  this->FileName = 0;
  this->SetNumberOfInputPorts(0);
+
+///TODO: lots of copy and paste here, remove, but first need to figure out how exactly to deal with vtkSmartPointer template class in header file
   // Allocate objects to hold points and vertex cells.
  points = vtkSmartPointer<vtkPoints>::New();
  verts = vtkSmartPointer<vtkCellArray>::New();
  // Allocate Scalars
- mass_scalars = vtkSmartPointer<vtkDoubleArray>::New();//added this
+ //mass
+ mass_scalars = vtkSmartPointer<vtkFloatArray>::New();
  mass_scalars->SetNumberOfComponents(1);
  mass_scalars->SetName("mass");
+//potential
+ phi_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ phi_scalars->SetNumberOfComponents(1);
+ phi_scalars->SetName("potential");
+ //rho
+ rho_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ rho_scalars->SetNumberOfComponents(1);
+ rho_scalars->SetName("rho");
+ //temp
+ temp_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ temp_scalars->SetNumberOfComponents(1);
+ temp_scalars->SetName("temperature");
+ //smooth
+ hsmooth_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ hsmooth_scalars->SetNumberOfComponents(1);
+ hsmooth_scalars->SetName("hsmooth");
+ //metals
+ metals_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ metals_scalars->SetNumberOfComponents(1);
+ metals_scalars->SetName("metals");
+ //softening
+ eps_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ eps_scalars->SetNumberOfComponents(1);
+ eps_scalars->SetName("softening");
+ //tform
+ tform_scalars = vtkSmartPointer<vtkFloatArray>::New();
+ tform_scalars->SetNumberOfComponents(1);
+ tform_scalars->SetName("tform");
 }
 
 //----------------------------------------------------------------------------
@@ -59,14 +95,39 @@ void vtkTipsyReader::PrintSelf(ostream& os, vtkIndent indent)
 }
 
 //----------------------------------------------------------------------------
-void vtkTipsyReader::ReadBaseParticle(TipsyBaseParticle& baseParticle) {
-	x[0]=baseParticle.pos[0];
-	x[1]=baseParticle.pos[1];
-	x[2]=baseParticle.pos[2];
-	vtkIdType id = points->InsertNextPoint(x);
-    verts->InsertNextCell(1, &id);
-	mass_scalars->InsertNextValue(baseParticle.mass);
+void vtkTipsyReader::ReadParticle(TipsyBaseParticle& baseParticle) {
+  vtkIdType id = points->InsertNextPoint(baseParticle.pos[0],baseParticle.pos[1],baseParticle.pos[2]);
+  verts->InsertNextCell(1, &id);
+  mass_scalars->SetValue(id,baseParticle.mass);
+  phi_scalars->SetValue(id,baseParticle.phi);
+/*
+  switch:
+  	case typeid(baseParticle) == TipsyGasParticle: //better way than typeid, pass by string? opinion of expert
+	  ReadGasParticle(id,baseParticle);
+  	case typeid(baseParticle) == TipsyDarkParticle:
+      ReadDarkParticle(id,baseParticle);
+  	case typeid(baseParticle) == TipsyStarParticle:
+  	  ReadStarParticle(id,baseParticle);
+*/
 }
+void vtkTipsyReader::ReadGasParticle(vtkIdType id,TipsyGasParticle& gasParticle) {
+  rho_scalars->SetValue(id, gasParticle.rho);
+  temp_scalars->SetValue(id, gasParticle.temp);
+  hsmooth_scalars->SetValue(id, gasParticle.hsmooth);
+  metals_scalars->SetValue(id, gasParticle.metals);
+}
+
+
+void vtkTipsyReader::ReadDarkParticle(vtkIdType id,TipsyDarkParticle& darkParticle) {
+  eps_scalars->SetValue(id, darkParticle.eps);
+}
+
+void vtkTipsyReader::ReadStarParticle(vtkIdType id,TipsyStarParticle& starParticle) {
+  eps_scalars->SetValue(id, starParticle.eps);
+  metals_scalars->SetValue(id, starParticle.metals);
+  tform_scalars->SetValue(id, starParticle.metals);
+}
+
 
 int vtkTipsyReader::RequestData(vtkInformation*,
                                        vtkInformationVector**,
@@ -94,15 +155,15 @@ int vtkTipsyReader::RequestData(vtkInformation*,
   // Read every particle and add their position to be displayed
   for( i=0; i<h.h_nSph;  i++ ) {
 	in >> g;
-	ReadBaseParticle(g);
+	ReadParticle(g);
   }
   for( i=0; i<h.h_nDark; i++ ) { 
 	in >> d;
-	ReadBaseParticle(d);
+	ReadParticle(d);
   }
   for( i=0; i<h.h_nStar; i++) {
 	in >> s;
-	ReadBaseParticle(s);
+	ReadParticle(s);
   }
   // Close the file.
   in.close();
@@ -116,6 +177,7 @@ int vtkTipsyReader::RequestData(vtkInformation*,
   output->SetPoints(points);
   output->SetVerts(verts); 
   output->GetPointData()->SetScalars(mass_scalars);
+  output->GetPointData()->SetScalars(phi_scalars);
   return 1;
 
 //TODO: memory management, may need to call things like mass_scalars->Delete(); points->Delete(); verts->Delete(); etc.
