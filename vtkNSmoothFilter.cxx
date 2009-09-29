6f29dee84409892d85128d503d80ae808ed27d2b
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-09-29 09:29:44 +0000

    added mass smoothing capabilities, testing. will next add support for other scalar components, then finally volume for use in density smoothing

--- vtkNSmoothFilter.cxx
+++ vtkNSmoothFilter.cxx
@@ -15,6 +15,7 @@
 #include "vtkPolyData.h"
 #include "vtkPoints.h"
 #include "vtkGenericPointIterator.h"
+#include "vtkDoubleArray.h"
 
 vtkCxxRevisionMacro(vtkNSmoothFilter, "$Revision: 1.72 $");
 vtkStandardNewMacro(vtkNSmoothFilter);
@@ -69,42 +70,56 @@ int vtkNSmoothFilter::RequestData(vtkInformation*,
 	// copies the point attributes
   output->CopyAttributes(input);
 	// Building the Kd tree
-  vtkDebugMacro("1. Building Kd tree.");
+  vtkDebugMacro("1a. Building Kd tree.");
 	vtkSmartPointer<vtkKdTree> pointTree = vtkSmartPointer<vtkKdTree>::New();
 		pointTree->BuildLocatorFromPoints(output);
+	vtkDebugMacro("1b. Allocating arrays to store our smoothed values.");
+	// allocating an array to store the smoothed mass
+	vtkSmartPointer<vtkDoubleArray> smoothedMassArray = vtkSmartPointer<vtkDoubleArray>::New();
+  	smoothedMassArray->SetNumberOfComponents(output->GetPoints()->GetNumberOfPoints());
+  	smoothedMassArray->SetName("smoothed mass");
+		smoothedMassArray->SetNumberOfTuples(1);
   vtkDebugMacro("2. Calculating the smoothed quantities we are interested in.");
 	for(int id = 0; id < output->GetPoints()->GetNumberOfPoints(); ++id)
 		{
 		double nextPoint[3]; 
 		output->GetPoints()->GetPoint(id,nextPoint);
 		vtkDebugMacro("next point is " << nextPoint[0] << ","<< nextPoint[1] << ","<< nextPoint[2]);
-		//finding the closest N points
+		// finding the closest N points
 		vtkDebugMacro("2. Finding the closeset N points");
 		vtkSmartPointer<vtkIdList> closestNPoints = vtkSmartPointer<vtkIdList>::New();
 		pointTree->FindClosestNPoints(this->NeighborNumber,nextPoint,closestNPoints);
 		vtkErrorMacro("found " << closestNPoints->GetNumberOfIds() << " closest points");
-		//looping over the closestNPoints
-		for(int j = 0; j < closestNPoints->GetNumberOfIds(); ++j)
+		// looping over the closestNPoints, only if we have more neighbors than ourselves
+		if(closestNPoints->GetNumberOfIds()>0)
 			{
-			double neighborPoint[3];
-			vtkIdType neighborPointId = closestNPoints->GetId(j);
-			output->GetPoints()->GetPoint(neighborPointId,neighborPoint);
-			vtkErrorMacro("the " << j <<"th nearest point coordiates are (" << neighborPoint[0] << "," << neighborPoint[1] << "," << neighborPoint[2] << ")");
-			//extracting the mass
-			double mass[1];
-			output->GetPointData()->SetActiveScalars("mass");
-			output->GetPointData()->GetScalars()->GetTuple(neighborPointId,mass);
-			vtkErrorMacro("it has mass " << mass[0]);
+			double totalMass;
+			for(int j = 0; j < closestNPoints->GetNumberOfIds(); ++j)
+				{
+				double neighborPoint[3];
+				vtkIdType neighborPointId = closestNPoints->GetId(j);
+				output->GetPoints()->GetPoint(neighborPointId,neighborPoint);
+				vtkDebugMacro("the " << j <<"th nearest point coordiates are (" << neighborPoint[0] << "," << neighborPoint[1] << "," << neighborPoint[2] << ")");
+				//extracting the mass
+				double mass[1];
+				output->GetPointData()->SetActiveScalars("mass");
+				output->GetPointData()->GetScalars()->GetTuple(neighborPointId,mass);
+				vtkDebugMacro("it has mass " << mass[0]);
+				totalMass+=mass[1];
+				}
+			//storing the smoothed mass in the output vector
+			double smoothedMass=totalMass/closestNPoints->GetNumberOfIds();
+		  smoothedMassArray->SetValue(id, smoothedMass);
+			//finding the average of each property we are interested in by dividing by #closestNPoints
+			//the volume is a sphere around nextPoint with radius of the last in the list of the closestNpoints
+			//so 4/3 pi r^3 where r=sqrt((nextPoint->x-nextPoint->x)^2+(nextPoint->y-nextPoint->y)^2+(nextPoint->z-nextPoint->z)^2)
 			}
-		//finding the average of each property we are interested in by dividing by #closestNPoints
-		//the volume is a sphere around nextPoint with radius of the last in the list of the closestNpoints
-		//so 4/3 pi r^3 where r=sqrt((nextPoint->x-nextPoint->x)^2+(nextPoint->y-nextPoint->y)^2+(nextPoint->z-nextPoint->z)^2)
 		}
 	vtkDebugMacro("3. Storing smoothed quantities in output.");
+	//storing the output vector in the output
+	output->GetPointData()->AddArray(smoothedMassArray);
 	// Finally, some memory management
   output->Squeeze();
-  // Memory management of the Kd-tree we have built. 
-	// TODO: Could later consider outputting this structure for further analysis.
   vtkDebugMacro("4. Smoothing calculation sucessful, deleting Kd tree.");
   return 1;
 }
