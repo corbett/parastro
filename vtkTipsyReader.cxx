494763f5465f92dfcfb4b72d75029201186cc537
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-09-30 14:21:20 +0000

    removed polydatahelper architecture, need to investigate more how paraview handles library additions.

--- vtkTipsyReader.cxx
+++ vtkTipsyReader.cxx
@@ -216,6 +216,33 @@ void vtkTipsyReader::AllocateAllTipsyVariableArrays(TipsyHeader& tipsyHeader,vtk
 	// the default vectors to be displayed
   AllocateDataArray(output,"velocity",3,tipsyHeader.h_nBodies);
 }
+//----------------------------------------------------------------------------
+void vtkTipsyReader::AllocateDataArray(vtkPolyData* output, const char* arrayName, int numComponents, int numTuples)
+{
+	vtkSmartPointer<vtkFloatArray> dataArray=vtkSmartPointer<vtkFloatArray>::New();
+  	dataArray->SetNumberOfComponents(numComponents);
+  	dataArray->SetName(arrayName);
+		dataArray->SetNumberOfTuples(numTuples);
+  output->GetPointData()->SetScalars(dataArray);
+}
+//----------------------------------------------------------------------------
+vtkIdType vtkTipsyReader::SetPointValue(vtkPolyData* output,float pos[3])
+{
+	vtkIdType id=output->GetPoints()->InsertNextPoint(pos);
+	output->GetVerts()->InsertNextCell(1, &id);
+	return id;
+}
+
+//----------------------------------------------------------------------------
+void vtkTipsyReader::SetDataValue(vtkPolyData* output, const char* arrayName, vtkIdType& id,float* data)
+{
+	output->GetPointData()->GetArray(arrayName)->SetTuple(id,data);
+}
+
+void vtkTipsyReader::GetDataValue(vtkPolyData* output, const char* arrayName, vtkIdType& id,double* data)
+{
+	output->GetPointData()->GetArray(arrayName)->GetTuple(id,data);
+}
 
 //----------------------------------------------------------------------------
 int vtkTipsyReader::RequestData(vtkInformation*,
--- vtkTipsyReader.h
+++ vtkTipsyReader.h
@@ -11,7 +11,6 @@ Modified from vtkSimplePointsReader and from Doug Potter's Tipsylib
 #include "tipsylib/ftipsy.hpp" // needed for functions which take Tipsy particles as arguments
 #include "vtkSmartPointer.h" // needed for the functions to initialize arrays
 #include "vtkPolyData.h" // needed as most helper functions modify output which is vtkPolyData
-#include "PolyDataHelpers.h" // needed as some functions depend upon
 #include <queue> // needed for FIFO queue used to store marked particles
 using std::queue;
 class VTK_IO_EXPORT vtkTipsyReader : public vtkPolyDataAlgorithm
@@ -69,5 +68,22 @@ private:
 	// Description:
 	// allocates all vtk arrays for Tipsy variables and places them in the output vector
 	void AllocateAllTipsyVariableArrays(TipsyHeader& tipsyHeader,vtkPolyData* output);
+	// Description:
+	// sets the point vertices in the output vector, assigning the point a unique id
+	// and places one point per cell
+	vtkIdType SetPointValue(vtkPolyData* output,float pos[3]);
+	// Description:
+	// sets the data value in the output vector in array arrayName at position id to data.
+	void SetDataValue(vtkPolyData* output, const char* arrayName, vtkIdType& id,float* data);
+	// Description:
+	// points the double* data to the value in the output vector in array arrayName at position id
+	// would like to be float, but the version of vtk I am working with does not support 
+	void GetDataValue(vtkPolyData* output, const char* arrayName, vtkIdType& id,double* data);
+	// Description:
+	// create a vtkDataArray with the  name arrayName, number of components 
+	// numComponents and number of tuples numTuples of type T
+	// e.g. AllocateFloatArray("density",1,100) creates a array of 100 scalar float densities
+	// AllocateFloatArray("velocity",3,100) creates a array of 100 vector float velocities
+	void AllocateDataArray(vtkPolyData* output, const char* arrayName, int numComponents, int numTuples);
 };
 #endif
