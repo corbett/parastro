daa7d33ad40199a7fa4c9bab09c963c210068035
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-10-10 14:23:50 +0000

    committing added library to help the profile command find the virial radius. right now using identical header/code to Doug/Joachim illinois from pkdgrav/master.c but changed it's name to IllinoisRootFinder. Implemented a function OverDensityInSphere which calculates, the difference between the density in a sphere and the critical density as will be specified in the input to the Profile command

--- astrovizhelpers/ProfileHelpers.cxx
+++ astrovizhelpers/ProfileHelpers.cxx
@@ -8,3 +8,103 @@
 #include "DataSetHelpers.h"
 #include <cmath>
 //----------------------------------------------------------------------------
+double IllinoisRootFinder(double (*func)(double,void *),void *ctx,\
+											double r,double s,double xacc,double yacc,\
+											int *pnIter) 
+{
+	// This code copied from Doug Potter's and Joachim Stadel's
+	// pkdgrav, class master.c, method illinois
+  const int maxIter = 100;
+  double t,fr,fs,ft,phis,phir,gamma;
+  int i;
+
+  fr = func(r,ctx);
+  fs = func(s,ctx);
+  assert(fr*fs < 0);
+  t = (s*fr - r*fs)/(fr - fs);
+
+  for(i=0; i<maxIter && fabs(t-s) > xacc; ++i) 
+		{
+		ft = func(t,ctx);
+		if (fabs(ft)<=yacc)
+		 {
+		 break;
+		 }
+		if(ft*fs<0) 
+			{
+	   	/*
+	   	** Unmodified step.
+	   	*/
+	   	r = s;
+	   	s = t;
+	   	fr = fs;
+	   	fs = ft;
+			}
+		else 
+			{
+	   	/*
+	   	** Modified step to make sure we do not retain the 
+	   	** endpoint r indefinitely.
+	   	*/
+			phis = ft/fs;
+	    phir = ft/fr;
+	    gamma = 1 - (phis/(1-phir));  /* method 3, for true illinois gamma=0.5*/
+	    if(gamma < 0) 
+				{
+				gamma = 0.5;
+	   		}
+	   	fr *= gamma;
+	   	s = t;
+	   	fs = ft;
+			}
+		t = (s*fr - r*fs)/(fr - fs);
+		}	
+		
+  if(pnIter)
+		{
+		*pnIter = i;
+		}
+		
+  return(t);
+}
+
+double OverDensityInSphere(double r,void* inputLocatorInfo)
+{
+	LocatorInfo* locatorInfo = static_cast<LocatorInfo*>(inputLocatorInfo);
+	vtkSmartPointer<vtkIdList> pointsInRadius = \
+																vtkSmartPointer<vtkIdList>::New();
+	locatorInfo->locator->FindPointsWithinRadius(r,locatorInfo->center,\
+																							pointsInRadius);
+	// calculating the average mass, dividing this by the volume of the sphere
+	// to get the density
+	double totalMass;
+	for(int pointLocalId = 0; \
+			pointLocalId < pointsInRadius->GetNumberOfIds(); \
+			++pointLocalId)
+		{
+		vtkIdType pointGlobalId = pointsInRadius->GetId(pointLocalId);
+		vtkPointSet* dataSet=\
+								vtkPointSet::SafeDownCast(locatorInfo->locator->GetDataSet());
+		double* nextPoint=GetPoint(dataSet,pointGlobalId);
+		// extracting the mass
+		// has to be double as this version of VTK doesn't have 
+		double* mass=GetDataValue(dataSet,\
+															"mass",pointGlobalId);
+		totalMass+=mass[0];
+		// Finally, some memory management
+		delete [] mass;
+		delete [] nextPoint;
+		}
+		
+		// Returning the density minus the critical density
+		return totalMass/(4./3*M_PI*pow(r,3)) - locatorInfo->criticalDensity;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- astrovizhelpers/ProfileHelpers.h
+++ astrovizhelpers/ProfileHelpers.h
@@ -7,7 +7,35 @@
 #include "vtkIdTypeArray.h"
 #include "vtkIdList.h"
 #include "vtkCell.h"
+#include "vtkPointLocator.h"
 #include "vtkCellArray.h"
 #include "vtkFloatArray.h"
+// Uses the Illinois root finding method to find the root of the function
+// func. The root must lie between r and s. Root is returned when it is found 
+// within the accuracy xacc, yacc. pnIter indicates how many iterations the
+// algorithm took to converge
+// have to wrap as it is a template function
+//BTX
+double IllinoisRootFinder(double (*func)(double,void *),void *ctx,double r,double s,double xacc,double yacc,int *pnIter);
+//ETX
+// Description:
+// This assumes locatorStruct is a LocatorInfo struct, which contains
+// a locator for a given vtkdataset, a center from which to calculate
+// the volume 
+// Given a radius, a center, calculates the density of within the sphere
+// of radius r around the center and subtracts the critical density of 
+// the user's choice.
+// 
+double OverDensityInSphere(double r, void* locatorStruct);
 
-
+// Description:
+// The locator struct is an pointLocatorStruct containing:
+// .locator which is a vtkPointLocator
+// .center  which is a double[3]
+// .criticalDensity which is a double
+struct LocatorInfo 
+{
+	vtkPointLocator* locator;
+	double center[3];
+	double criticalDensity;
+};
