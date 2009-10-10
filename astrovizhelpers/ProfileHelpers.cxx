edbcfad751452d42133ea1a30a052bbd473a1ce6
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-10-10 10:48:03 +0000

    creating the class/library profile helpers to assist with root finding and other tasks necessary for profiling. putting it outside of the vtk class as it may be useful for other classes later

---
+++ astrovizhelpers/ProfileHelpers.cxx
@@ -0,0 +1,75 @@
+#include "DataSetHelpers.h"
+#include "vtkPointData.h"
+#include "vtkCellArray.h"
+#include "vtkFloatArray.h"
+#include "vtkDoubleArray.h"
+#include "vtkIntArray.h"
+#include "vtkSmartPointer.h"
+#include <cmath>
+//----------------------------------------------------------------------------
+void AllocateDataArray(vtkPointSet* output, const char* arrayName,\
+ 			int numComponents, int numTuples)
+{
+	vtkSmartPointer<vtkFloatArray> dataArray=\
+		vtkSmartPointer<vtkFloatArray>::New();
+  	dataArray->SetNumberOfComponents(numComponents);
+  	dataArray->SetName(arrayName);
+		dataArray->SetNumberOfTuples(numTuples);
+  output->GetPointData()->AddArray(dataArray);
+}
+//----------------------------------------------------------------------------
+void AllocateDoubleDataArray(vtkPointSet* output, const char* arrayName,\
+ 			int numComponents, int numTuples)
+{
+	vtkSmartPointer<vtkDoubleArray> dataArray=\
+		vtkSmartPointer<vtkDoubleArray>::New();
+  	dataArray->SetNumberOfComponents(numComponents);
+  	dataArray->SetName(arrayName);
+		dataArray->SetNumberOfTuples(numTuples);
+  output->GetPointData()->AddArray(dataArray);
+}
+
+//----------------------------------------------------------------------------
+vtkIdType SetPointValue(vtkPolyData* output,float pos[])
+{
+	vtkIdType id=output->GetPoints()->InsertNextPoint(pos);
+	output->GetVerts()->InsertNextCell(1, &id);
+	return id;
+}
+
+//----------------------------------------------------------------------------
+double* GetPoint(vtkPointSet* output,vtkIdType id)
+{
+	double* nextPoint=new double[3]; 
+	output->GetPoints()->GetPoint(id,nextPoint);
+	return nextPoint;
+}
+
+
+//----------------------------------------------------------------------------
+void SetDataValue(vtkPointSet* output, const char* arrayName,\
+			vtkIdType id,float data[])
+{
+	output->GetPointData()->GetArray(arrayName)->SetTuple(id,data);
+}
+//----------------------------------------------------------------------------
+void SetDataValue(vtkPointSet* output, const char* arrayName,\
+			vtkIdType id,double data[])
+{
+	output->GetPointData()->GetArray(arrayName)->SetTuple(id,data);
+}
+//----------------------------------------------------------------------------
+double* GetDataValue(vtkPointSet* output, const char* arrayName,\
+ 					vtkIdType id)
+{
+	double* data=new double[3];
+	output->GetPointData()->GetArray(arrayName)->GetTuple(id,data);
+	return data;
+}
+//----------------------------------------------------------------------------
+double ComputeRadialDistance(double pointOne[],double pointTwo[])
+{
+	return sqrt(pow(pointOne[0]-pointTwo[0],2) \
+					+pow(pointOne[1]-pointTwo[1],2) \
+					+pow(pointOne[2]-pointTwo[2],2));
+}
