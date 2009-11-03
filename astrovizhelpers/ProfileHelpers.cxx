df2a28feaf05771ddbcb9738134cf2ddd1c5cafe
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-11-03 10:18:28 +0000

    playing around with colors

--- astrovizhelpers/ProfileHelpers.cxx
+++ astrovizhelpers/ProfileHelpers.cxx
@@ -482,7 +482,7 @@ void DisplayVectorsAsLines(vtkPointSet* input, vtkPolyData* output,
 		colors->SetNumberOfComponents(3);
 		colors->SetName("Colors");
 		// creting red, green, and blue colors one for each line
-		unsigned char red[3];={255,0,0};
+		unsigned char red[3];
 		red[0]=255;red[1]=0;red[2]=0;
 		unsigned char green[3];
 		green[0]=0;green[1]=255;green[2]=0;
