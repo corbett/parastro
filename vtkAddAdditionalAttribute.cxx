79b8ff40e41bc77b5c2ebd541c17531899f34396
corbett <corbett@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2009-12-06 12:53:25 +0000

    debugging basic parallel add attribute

--- vtkAddAdditionalAttribute.cxx
+++ vtkAddAdditionalAttribute.cxx
@@ -82,12 +82,12 @@ int vtkAddAdditionalAttribute::ReadAdditionalAttributeFile(
 	if(globalIdArray->GetNumberOfTuples() == \
 		output->GetPoints()->GetNumberOfPoints())
 		{
-		// TODO: add back in
-		/*
 			
 		// read additional attribute for all particles
 		AllocateDataArray(output,this->AttributeName,1,
 			output->GetPoints()->GetNumberOfPoints());		
+		// TODO: add back in
+		/*	
 		double attributeData;
 		// TODO: remove this; should be the number of bodies...
 		attributeInFile >> attributeData;
