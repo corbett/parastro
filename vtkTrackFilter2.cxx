2845609a687ca0a753aa00f3a03f3f5c173b9302
rafik <rafik@2e4b4254-30d0-11de-8d27-b1395bdb903a>
2011-02-17 18:42:26 +0000

    emptz version of trackfilter2

---
+++ vtkTrackFilter2.cxx
@@ -0,0 +1,91 @@
+/*=========================================================================
+
+  Program:   
+  Module:    vtkTrackFilter2.cxx
+
+  Copyright (c) Rafael Küng
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkTrackFilter2.h"
+
+#include "AstroVizHelpersLib/AstroVizHelpers.h"
+
+#include "vtkPolyData.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+
+#include "vtkTimerLog.h"
+#include "vtkSmartPointer.h"
+#include "vtkDataSetAttributes.h"
+
+#include "vtkSmartPointer.h"
+#include "vtkCellArray.h"
+#include "vtkIdList.h"
+#include "vtkPointData.h"
+#include "vtkDataArray.h"
+#include "vtkPolyLine.h"
+
+#include <vector>
+
+
+vtkCxxRevisionMacro(vtkTrackFilter2, "$Revision: 0.1 $");
+vtkStandardNewMacro(vtkTrackFilter2);
+
+//----------------------------------------------------------------------------
+vtkTrackFilter2::vtkTrackFilter2()
+{
+	this->SetNumberOfInputPorts(1);
+	this->SetNumberOfOutputPorts(1);
+
+}
+
+//----------------------------------------------------------------------------
+vtkTrackFilter2::~vtkTrackFilter2()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkTrackFilter2::PrintSelf(ostream& os, vtkIndent indent)
+{
+	this->Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+int vtkTrackFilter2::FillInputPortInformation(int port, vtkInformation* info)
+{
+	this->Superclass::FillInputPortInformation(port, info);
+	info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+	return 1;
+}
+//----------------------------------------------------------------------------
+int vtkTrackFilter2::FillOutputPortInformation(
+	int vtkNotUsed(port), vtkInformation* info)
+{
+	info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPolyData");
+	return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkTrackFilter2::RequestData(vtkInformation*,
+								vtkInformationVector** inputVector,
+								vtkInformationVector* outputVector)
+{
+	vtkSmartPointer<vtkTimerLog> timer = vtkSmartPointer<vtkTimerLog>::New();
+	timer->StartTimer();
+
+	vtkInformation* outInfo = outputVector->GetInformationObject(0);
+	vtkPolyData* output = vtkPolyData::GetData(outputVector);
+	vtkPolyData* input = vtkPolyData::GetData(inputVector[0]);
+
+
+	timer->StopTimer();
+	vtkErrorMacro(" track selection took: " << timer->GetElapsedTime() << " s");
+	
+	return 1;
+}
---
+++ vtkTrackFilter2.h
@@ -0,0 +1,54 @@
+/*=========================================================================
+
+  Program:   
+  Module:    vtkTrackFilter2.h
+
+  Copyright (c) Rafael Küng
+  All rights reserved.
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkTrackFilter2 - Filter Tracks
+// .SECTION Description
+// vtkTrackFilter2 allows to select Lines, where a certain PointData Value meets specified condition
+
+#ifndef __vtkTrackFilter2_h
+#define __vtkTrackFilter2_h
+
+#include "vtkPolyDataAlgorithm.h" // superclass
+#include "vtkSmartPointer.h"
+#include "vtkPolyData.h"
+#include "vtkPoints.h"
+#include "vtkCellArray.h"
+#include "vtkFloatArray.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include <vtkstd/vector>
+
+class VTK_EXPORT vtkTrackFilter2 :  public vtkPolyDataAlgorithm
+{
+public:
+	static vtkTrackFilter2* New();
+	//vtkTypeMacro(vtkTrackFilter2, vtkPolyDataAlgorithm);
+	vtkTypeRevisionMacro(vtkTrackFilter2,vtkPolyDataAlgorithm);
+
+	void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+	vtkTrackFilter2();
+	~vtkTrackFilter2();
+
+	virtual int FillInputPortInformation(int port, vtkInformation* info);
+	virtual int FillOutputPortInformation(int vtkNotUsed(port),	vtkInformation* info);
+	virtual int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+
+
+private:
+	vtkTrackFilter2(const vtkTrackFilter2&);  // Not implemented.
+	void operator=(const vtkTrackFilter2&);  // Not implemented.
+
+};
+
+#endif
